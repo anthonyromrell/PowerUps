1b055e444cadba31c659725fd33b2eda1e461643
Anthony Romrell <anthony@artisandream.com>
2018-01-09 17:00:48 -0700

    Adding PowerUps

---
+++ PowerUpBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PowerUpBase : ScriptableObject {
+
+	public Sprite PowerUpSprite;
+	public float PowerLevel = 1;
+
+	public Sprite UpdateSprite () {
+		return PowerUpSprite;
+	}
+}
---
+++ PowerUpBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class PowerUpBehaviour : MonoBehaviour {
+
+	public PowerUpBase PowerUpSO;
+
+	void OnTriggerEnter(Collider other)
+	{
+		print(PowerUpSO.PowerLevel);
+	}
+
+	[ContextMenu("Add Sprite")]
+	void Start()
+	{
+		GetComponent<SpriteRenderer>().sprite = PowerUpSO.UpdateSprite();
+	}
+}
